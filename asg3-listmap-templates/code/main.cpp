7f35ba576f7f62e67fbff9a69a728f575360724e
Warner Scheibe <wscheibe@ucsc.edu>
2020-11-21 15:51:00 -0800

    Update main.cpp

--- asg3-listmap-templates/code/main.cpp
+++ asg3-listmap-templates/code/main.cpp
@@ -59,12 +59,12 @@ int main (int argc, char** argv) {
          throw "line ignored";
          continue;
       }
-      if (regex_search (line, result, key_value_regex)) {
-         str_str_pair pair (result[1], result[2]); //declared here to make sure no errors
-          
-          test.insert(pair);
-          
-      }
+//      if (regex_search (line, result, key_value_regex)) {
+//         str_str_pair pair (result[1], result[2]); //declared here to make sure no errors
+//          
+//          test.insert(pair);
+//          
+//      }
       else if (regex_search (line, result, trimmed_regex)) {
          cout << "query: \"" << result[1] << "\"" << endl;
       }
